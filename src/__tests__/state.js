71598269dd1390bc2a605e0921db6e214d281490
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-30 14:41:02 -0400

    finish 10 of 41 queryBy RTL

--- src/__tests__/state.js
+++ src/__tests__/state.js
@@ -5,17 +5,23 @@ import {FavoriteNumber} from '../favorite-number'
 
 // incredibly readable; it just reads itself
 test('entering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole, rerender, debug} = render(
+  const {getByLabelText, getByRole, rerender, queryByRole} = render(
     <FavoriteNumber />,
   )
   const input = getByLabelText(/favorite number/i)
   user.type(input, '10') // simulates onKeyDown; onKeyUP; onChange
   // fireEvent.change(input, {target: {value: '10'}}) //input change is bit too vague
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
-  debug() // error div showing here as expected
+  // debug() // error div showing here as expected
   rerender(<FavoriteNumber max={10} />) // simulates props being updated
-  debug() // error div not showing here as expected
+  // debug() // error div not showing here as expected
+  expect(queryByRole('alert')).toBeNull() // it reads like it should work but it doesn't
+  // RTL errors out if getByBlah('foo') can't find foo
 })
+
+// queryBy vs getBy
+// use queryBy: when trying to prove that something doesn't exist
+// use getBy: for everything else
 
 /*
 <body>
