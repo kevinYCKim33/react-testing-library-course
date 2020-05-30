13f49a09b43b0af2b41d6c88ad212dd53d55e900
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-30 14:33:41 -0400

    finish 9 of 41 rerender RTL

--- src/__tests__/state.js
+++ src/__tests__/state.js
@@ -5,11 +5,16 @@ import {FavoriteNumber} from '../favorite-number'
 
 // incredibly readable; it just reads itself
 test('entering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole} = render(<FavoriteNumber />)
+  const {getByLabelText, getByRole, rerender, debug} = render(
+    <FavoriteNumber />,
+  )
   const input = getByLabelText(/favorite number/i)
   user.type(input, '10') // simulates onKeyDown; onKeyUP; onChange
   // fireEvent.change(input, {target: {value: '10'}}) //input change is bit too vague
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+  debug() // error div showing here as expected
+  rerender(<FavoriteNumber max={10} />) // simulates props being updated
+  debug() // error div not showing here as expected
 })
 
 /*
