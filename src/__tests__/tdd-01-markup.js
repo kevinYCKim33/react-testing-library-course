bc27d16353914f1e81d6c275971155b962b8646c
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-06-01 11:27:32 -0400

    finish 19 of 41 TDD basic

--- src/__tests__/tdd-01-markup.js
+++ src/__tests__/tdd-01-markup.js
@@ -1,10 +1,11 @@
+// 19 of 41 Test Drive the Development of a React Form with React Testing Library
 import React from 'react'
 import {render} from '@testing-library/react'
 import {Editor} from '../post-editor-01-markup'
 
 test('renders a form with title, content, tags, and a submit button', () => {
   const {getByLabelText, getByText} = render(<Editor />)
-  getByLabelText(/title/i)
+  getByLabelText(/title/i) // two birds in one stone...is there a label? is there an input associated to that label
   getByLabelText(/content/i)
   getByLabelText(/tags/i)
   getByText(/submit/i)
